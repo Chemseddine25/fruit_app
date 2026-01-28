1109b7222dc30f13021365f4fc54b51a5cebb841
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-01-28 15:15:27 +0100

    Login Bloc Consumer

---
+++ lib/features/auth/presentation/views/widgts/login_bloc_consumer.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_app/core/helper_functions/show_error_bar.dart';
+import 'package:fruit_app/features/auth/presentation/cubits/login/login_cubit.dart';
+import 'package:fruit_app/features/auth/presentation/views/widgts/login_view_body.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+class LoginBlocConsumer extends StatelessWidget {
+  const LoginBlocConsumer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginFailure) {
+          print(state.errorMessage);
+          showBar(context, message: state.errorMessage);
+        }
+        if (state is LoginSuccess) {
+          showBar(context, message: 'تم تسجيل الدخول بنجاح');
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+            inAsyncCall: state is LoginLoading ? true : false,
+            child: const LoginViewBody());
+      },
+    );
+  }
+}
