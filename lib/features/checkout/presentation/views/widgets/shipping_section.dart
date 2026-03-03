a76e865255088482a7f8b18697e36381bcae77bf
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-03 01:16:51 +0100

    feat: create shipping section

---
+++ lib/features/checkout/presentation/views/widgets/shipping_section.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_app/features/checkout/presentation/views/widgets/shipinig_view.dart';
+
+class ShippingSection extends StatefulWidget {
+  const ShippingSection({
+    super.key,
+  });
+
+  @override
+  State<ShippingSection> createState() => _ShippingSectionState();
+}
+
+class _ShippingSectionState extends State<ShippingSection> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 33,
+        ),
+        ShippingItem(
+          onTap: () {
+            setState(() {
+              selectedIndex = 0;
+            });
+          },
+          title: 'الدفع عند الاستلام',
+          subTitle: 'التسليم من المكان',
+          price: '40',
+          isSelected: selectedIndex == 0,
+        ),
+        const SizedBox(height: 16),
+        ShippingItem(
+          title: ' الدفع اونلاين',
+          subTitle: 'يرجي تحديد طريقه الدفع',
+          price: '40',
+          isSelected: selectedIndex == 1,
+          onTap: () {
+            setState(
+              () {
+                selectedIndex = 1;
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
