d4726a1a28aca059049d32118f9f7e0c9948a384
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-03 17:32:22 +0100

    feat: shipping adress widget

---
+++ lib/features/checkout/presentation/views/widgets/shipping_address_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fruit_app/core/utils/app_image.dart';
+import 'package:fruit_app/core/utils/test_style.dart';
+import 'package:fruit_app/features/checkout/presentation/views/widgets/payment_item.dart';
+
+class ShippingAddressWidget extends StatelessWidget {
+  const ShippingAddressWidget({
+    super.key,
+    required this.pageController,
+  });
+
+  final PageController pageController;
+  @override
+  Widget build(BuildContext context) {
+    return PaymentItem(
+      tile: 'عنوان التوصيل',
+      child: Row(
+        children: [
+          SvgPicture.asset(
+            Assets.assetsImagesLocation,
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            '  قسنطينة حي الاخوة عرفة رقم 416',
+            textAlign: TextAlign.right,
+            style: TextStyles.regular13.copyWith(
+              color: const Color(0xFF4E5556),
+            ),
+          ),
+          const Spacer(),
+          GestureDetector(
+            onTap: () {
+              pageController.animateToPage(1,
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeIn);
+            },
+            child: SizedBox(
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    Assets.assetsImagesEdit,
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    'تعديل',
+                    style: TextStyles.semiBold13.copyWith(
+                      color: const Color(0xFF949D9E),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
