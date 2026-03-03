50ecc64e1f4013de579783de2e1714f2950f5540
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-03 17:32:34 +0100

    feat: step item

--- lib/features/checkout/presentation/views/widgets/step_items.dart
+++ lib/features/checkout/presentation/views/widgets/step_items.dart
@@ -3,11 +3,12 @@ import 'package:fruit_app/features/checkout/presentation/views/widgets/active_st
 import 'package:fruit_app/features/checkout/presentation/views/widgets/in_active_step_item.dart';
 
 class StepItem extends StatelessWidget {
-  const StepItem(
-      {super.key,
-      required this.title,
-      required this.stepNumber,
-      required this.isSelected});
+  const StepItem({
+    super.key,
+    required this.title,
+    required this.stepNumber,
+    required this.isSelected,
+  });
 
   final String title;
   final String stepNumber;
@@ -16,7 +17,9 @@ class StepItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedCrossFade(
-        firstChild: ActiveStepItem(title: title),
+        firstChild: ActiveStepItem(
+          title: title,
+        ),
         secondChild: InActiveStepItem(title: title, stepNumber: stepNumber),
         crossFadeState:
             isSelected ? CrossFadeState.showFirst : CrossFadeState.showSecond,
