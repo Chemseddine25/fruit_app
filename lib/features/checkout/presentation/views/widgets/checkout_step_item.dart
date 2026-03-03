7a52551affaf1ddea2fb319c42e8f3d2a5d789fd
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-03 17:30:29 +0100

    feat: checkout step item

--- lib/features/checkout/presentation/views/widgets/checkout_step_item.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_step_item.dart
@@ -2,20 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:fruit_app/features/checkout/presentation/views/widgets/step_items.dart';
 
 class CheckoutStepItem extends StatelessWidget {
-  const CheckoutStepItem({super.key, required this.currentIndex});
-  final int currentIndex;
+  const CheckoutStepItem(
+      {super.key, required this.currentPageIndex, required this.controller});
+  final int currentPageIndex;
+  final PageController controller;
 
   @override
   Widget build(BuildContext context) {
     return Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: List.generate(getSteps.length, (index) {
-          return StepItem(
-              title: getSteps[index],
-              stepNumber: (index + 1).toString(),
-              isSelected: index <= currentIndex);
+          return Expanded(
+            child: GestureDetector(
+              onTap: () {
+                controller.animateToPage(index,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeIn);
+              },
+              child: StepItem(
+                title: getSteps[index],
+                stepNumber: (index + 1).toString(),
+                isSelected: index <= currentPageIndex,
+              ),
+            ),
+          );
         }));
   }
 }
 
-List<String> getSteps = ['الشحن', 'الدفع', 'تاكيد الطلب', 'تم التوصيل'];
+List<String> getSteps = [
+  'الشحن',
+  'تاكيد الطلب',
+  'الدفع',
+];
