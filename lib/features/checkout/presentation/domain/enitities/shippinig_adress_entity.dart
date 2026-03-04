36358ded8ca4cf7f0fb666cfa5c00b8422a29099
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-04 22:54:21 +0100

    feat: shipping adress

---
+++ lib/features/checkout/presentation/domain/enitities/shippinig_adress_entity.dart
@@ -0,0 +1,14 @@
+class ShpippingAdressEntity {
+  final String name;
+  final String email;
+  final String phone;
+  final String city;
+  final String adress;
+
+  ShpippingAdressEntity(
+      {required this.name,
+      required this.email,
+      required this.phone,
+      required this.city,
+      required this.adress});
+}
