d684250b4e0a4697d538bc5e63464aa7e3d956be
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-07 21:28:00 +0100

    feat : amount entity

---
+++ lib/features/checkout/presentation/domain/enitities/paypel_entities/amount_entity.dart
@@ -0,0 +1,23 @@
+import 'package:fruit_app/features/checkout/presentation/domain/enitities/order_entities/order_input_entity.dart';
+import 'package:fruit_app/features/checkout/presentation/domain/enitities/paypel_entities/details_entity.dart';
+
+class AmountEntity {
+  final String total;
+  final String currency;
+  DetailsEntity? details;
+
+  AmountEntity(
+      {required this.total,
+      required this.currency,
+      required DetailsEntity details});
+
+  factory AmountEntity.fromEntity(OrderInputEntity entity) {
+    return AmountEntity(
+      total: entity.calculateTotalPriceWithShipping().toStringAsFixed(2),
+      currency: "USD",
+      details: DetailsEntity.fromEntity(entity),
+    );
+  }
+
+  toJson() => {"total": total, "currency": currency, "details": details};
+}
