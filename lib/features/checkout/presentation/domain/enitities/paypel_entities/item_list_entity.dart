d7b8989317205c98965d4a008cfa1610f3d9b1b1
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-07 21:28:47 +0100

    feat item list entity

---
+++ lib/features/checkout/presentation/domain/enitities/paypel_entities/item_list_entity.dart
@@ -0,0 +1,17 @@
+import 'package:fruit_app/features/checkout/presentation/domain/enitities/paypel_entities/item_entity.dart';
+
+class ItemListEntity {
+  final List<ItemEntity> items;
+
+  ItemListEntity({required this.items});
+
+  factory ItemListEntity.fromEntity(List<ItemEntity> items) {
+    return ItemListEntity(items: items);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "items": items.map((e) => e.toJson()).toList(),
+    };
+  }
+}
