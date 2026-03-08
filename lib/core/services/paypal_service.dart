0847a6b98bb69b662bd56af2de62a471e30a3fdb
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-08 18:26:03 +0100

    feat: paypal service

--- lib/core/services/paypal_service.dart
+++ lib/core/services/paypal_service.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
+import 'package:fruit_app/core/utils/app_keys.dart';
 import 'package:fruit_app/features/checkout/presentation/domain/enitities/paypel_entities/paypel_enity.dart';
 
 class PaypalService {
-  static const String clientId =
-      "AVT7l1Y6XInknEGSqYcShAaoyE0GvJAPMjwetPX2NhdEdNtWFvyJBBD24u3NRbjLpq6WPtolUC0h_X6O";
-  static const String secretKey =
-      "EM5EhFXZS4FgGV9OXz7BzfGX4wdVA8JZPaeZgw6T73ePNihKKMl-SB04bGlp5L-hmWFh_Woxoc7n5B16";
+  static const String clientId = kpaypalIdClein;
+
+  static const String secretKey = kpaypalSecret;
+
+  bool _isSuccessCalled = false;
 
   void payWithPaypal({
     required BuildContext context,
@@ -15,6 +17,8 @@ class PaypalService {
     required Function(dynamic) onError,
     required Function() onCancel,
   }) {
+    _isSuccessCalled = false;
+
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (BuildContext context) => PaypalCheckoutView(
