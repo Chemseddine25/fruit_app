3621dfd0be49185840bd35e23245e43fdc5dba6a
Chemseddine25 <chemseddine.adjas.umba@gmail.com>
2026-03-08 18:26:28 +0100

    feat: add order cubit

--- lib/features/checkout/presentation/views/widgets/checkoutview bodt.dart
+++ lib/features/checkout/presentation/views/widgets/checkoutview bodt.dart
@@ -3,6 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_app/core/helper_functions/show_error_bar.dart';
 import 'package:fruit_app/core/widgets/custom_progress_hud.dart';
 import 'package:fruit_app/features/checkout/presentation/manager/add_order_cubit/add_order_cubit.dart';
+import 'package:fruit_app/features/checkout/presentation/views/checkout_view.dart';
+import 'package:fruit_app/features/main_view/presentation/views/cart_view.dart';
+import 'package:fruit_app/features/main_view/presentation/views/main_view.dart';
+import 'package:fruit_app/features/main_view/presentation/views/porduct_view.dart';
+import 'package:fruit_app/features/main_view/presentation/views/widgets/home_view_.dart';
 
 class AddOrderCubitBlocBuilder extends StatelessWidget {
   const AddOrderCubitBlocBuilder({super.key, required this.child});
@@ -14,6 +19,9 @@ class AddOrderCubitBlocBuilder extends StatelessWidget {
       listener: (context, state) {
         if (state is AddOrderSuccess) {
           showBar(context, message: 'تمت العملية بنجاح');
+          Future.delayed(const Duration(seconds: 1), () {
+            Navigator.pushReplacementNamed(context, MainView.routeName);
+          });
         }
 
         if (state is AddOrderFailure) {
